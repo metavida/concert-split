c465889c15f768a4bbc329197dbb4efc98eb28b4
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2010-05-12 10:44:38 -0400

    Simplified styling & finished inital work on listing of available concerts.

--- javascripts/base.js
+++ javascripts/base.js
@@ -1,31 +1,83 @@
+if(typeof(window.console) == 'undefined') {
+  window.console = {log:function(){}};
+} else if(typeof(window.console.log) == 'undefined') {
+  window.console.log = function(){};
+}
+
 jQuery(document).ready(function($) {
-	d = '';
-	var url = "http://github.com/api/v2/json/commits/list/metavida/concert-split/master?callback=?";
-	$.getJSON(url, function(data) {
+  $('#no_js').hide();
+  
+	var commit_url = 'http://github.com/api/v2/json/commits/list/metavida/concert-split/master?callback=?',
+	  tree_url = 'http://github.com/api/v2/json/tree/show/metavida/concert-split/',
+	  blob_url = 'http://github.com/api/v2/json/blob/show/metavida/concert-split/',
+	  html = '',
+	  waiting_on_a = 0;
+	waiting_on = 0;
+	
+	waiting_on++;
+	$.getJSON(commit_url, function(data) {
 		console.log(data);
-		url = "http://github.com/api/v2/json/tree/show/metavida/concert-split/"+ (data.commits[0].tree) +"?callback=?";
-		$.getJSON(url, function(data) {
+		
+		waiting_on++;
+		$.getJSON(tree_url + (data.commits[0].tree) +'?callback=?', function(data) {
 			console.log(data);
 			$.each(data.tree, function(concert) {
 				concert = data.tree[concert];
-				if(!concert.name.match(/^\./)) {
-					$('#songs').append("<div>"+concert.name+"</div>").show();
+				if(concert.type == 'tree') {
+				  html = '<h2>'+ concert.name +'</h2>';
+				  html += '<ul>';
+  				//console.log(concert.name + '=>' + concert.sha);
+  				waiting_on++;
+  				$.getJSON(tree_url + concert.sha +"?callback=?", function(data) {
+  				  //console.log(data);
+  					$.each(data.tree, function(concert) {
+  						concert = data.tree[concert];
+  						if(concert.type == 'tree') {
+  						  console.log(concert.name + '=>' + concert.sha);
+  						  waiting_on++;
+  						  $.getJSON(tree_url + concert.sha +"?callback=?", function(data) {
+  						    console.log(data);
+  						    var has_labels = false,
+  						      set_sha = '';
+            			$.each(data.tree, function(file) {
+            			  file = data.tree[file];
+  						      if(file.type == 'blob') {
+  						        if(file.name.match(/Audacity Labels/i)) {
+    						        has_labels = file.sha;
+  						        } else if(file.name.match(/Set List/i)) {
+  						          set_sha = file.sha;
+  						        }
+  						      }
+                  });
+                  if(has_labels && set_sha) {
+                    html += '<li><a href="'+ blob_url + has_labels +'">'+ concert.name + '</a>';
+                    //html += ' (<a href="'+ blob_url + set_sha +'">view set list</a>)';
+                    html += '</li>';
+					        } else {
+					          html += '<li class="no_labels">'+ concert.name;
+					          html += ' (awaiting set list)';
+					          html += '</li>';
+					        }
+                waiting_on--;
+                });
+  						}
+  					});
+  				waiting_on--;
+  				});
 				}
-				
-				url = "http://github.com/api/v2/json/tree/show/metavida/concert-split/"+ concert.name +"/"+ concert.sha +"?callback=?";
-				console.log(url);
-				$.getJSON(url, function(data) {
-					$.each(data.tree, function(concert) {
-						concert = data.tree[concert];
-						if(!concert.name.match(/^\./)) {
-							$('#songs').append("<div>"+concert.name+"</div>").show();
-						}
-					});
-				});
-				
 			});
+		waiting_on--;
 		});
+	waiting_on--;
 	});
+	
+	var waiting_id = setInterval(function() {
+	  if(waiting_on == 0) {
+	    clearInterval(waiting_id);
+	    html += '</ul>';
+    	$('#songs').append(html).show();
+	  }
+	}, 100);
 
   //$.each($('.post.link.github h3 a'), function() {
   //  var post = $(this).parents(".post");
