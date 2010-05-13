7118f390f2fe82f781624e23068dc483f44c7367
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2010-05-13 09:12:56 -0400

    Improved display progress when loading song data. Sorted concerts.

--- javascripts/base.js
+++ javascripts/base.js
@@ -1,3 +1,4 @@
+// In case I forget to comment out a console.log line.
 if(typeof(window.console) == 'undefined') {
   window.console = {log:function(){}};
 } else if(typeof(window.console.log) == 'undefined') {
@@ -10,33 +11,40 @@ jQuery(document).ready(function($) {
 	var commit_url = 'http://github.com/api/v2/json/commits/list/metavida/concert-split/master?callback=?',
 	  tree_url = 'http://github.com/api/v2/json/tree/show/metavida/concert-split/',
 	  blob_url = 'http://github.com/api/v2/json/blob/show/metavida/concert-split/',
+	  songs_el = $('#songs'),
+	  loading_el = $('#songs_loading'),
 	  html = '',
 	  waiting_on_a = 0;
 	waiting_on = 0;
 	
+	// Get the most recent commit so we know the SHA of the root tree
 	waiting_on++;
 	$.getJSON(commit_url, function(data) {
-		console.log(data);
+		//console.log(data);
 		
+		// Loop over the list of root directories, which should NPR shows.
 		waiting_on++;
 		$.getJSON(tree_url + (data.commits[0].tree) +'?callback=?', function(data) {
-			console.log(data);
+			//console.log(data);
 			$.each(data.tree, function(concert) {
 				concert = data.tree[concert];
 				if(concert.type == 'tree') {
-				  html = '<h2>'+ concert.name +'</h2>';
-				  html += '<ul>';
-  				//console.log(concert.name + '=>' + concert.sha);
+				  songs_el.append('<h2>'+ concert.name +'</h2><ul></ul>');
+				  songs_el.find('h2').last().append(loading_el);
+				  var ul_el = songs_el.find('ul').last();
+  				
+  				// Loop over directories for a single show, which should be individual concerts.
   				waiting_on++;
   				$.getJSON(tree_url + concert.sha +"?callback=?", function(data) {
   				  //console.log(data);
-  					$.each(data.tree, function(concert) {
+  					$.each(data.tree.sort(), function(concert) {
   						concert = data.tree[concert];
   						if(concert.type == 'tree') {
-  						  console.log(concert.name + '=>' + concert.sha);
+  						  
+  						  // Get the details for this concert.
   						  waiting_on++;
   						  $.getJSON(tree_url + concert.sha +"?callback=?", function(data) {
-  						    console.log(data);
+  						    //console.log(data);
   						    var has_labels = false,
   						      set_sha = '';
             			$.each(data.tree, function(file) {
@@ -50,14 +58,15 @@ jQuery(document).ready(function($) {
   						      }
                   });
                   if(has_labels && set_sha) {
-                    html += '<li><a href="'+ blob_url + has_labels +'">'+ concert.name + '</a>';
+                    html = '<li><a href="'+ blob_url + has_labels +'">'+ concert.name + '</a>';
                     //html += ' (<a href="'+ blob_url + set_sha +'">view set list</a>)';
                     html += '</li>';
 					        } else {
-					          html += '<li class="no_labels">'+ concert.name;
+					          html = '<li class="no_labels">'+ concert.name;
 					          html += ' (awaiting set list)';
 					          html += '</li>';
 					        }
+					        ul_el.append(html);
                 waiting_on--;
                 });
   						}
@@ -74,26 +83,7 @@ jQuery(document).ready(function($) {
 	var waiting_id = setInterval(function() {
 	  if(waiting_on == 0) {
 	    clearInterval(waiting_id);
-	    html += '</ul>';
-    	$('#songs').append(html).show();
+	    loading_el.hide();
 	  }
 	}, 100);
-
-  //$.each($('.post.link.github h3 a'), function() {
-  //  var post = $(this).parents(".post");
-  //  var url = $(this).attr('href');
-  //  var segments = url.split('/');
-  //  var repo = segments.pop();
-  //  var username = segments.pop();
-  //  $.getJSON("http://github.com/api/v2/json/repos/show/"+username+"/"+repo+"?callback=?", function(data){
-  //    var repo_data = data.repository;
-  //    if(repo_data) {
-  //      var watchers_link = $('<a>').addClass('watchers').attr('href', url+'/watchers').text(repo_data.watchers);
-  //      var forks_link = $('<a>').addClass('forks').attr('href', url+'/network').text(repo_data.forks);
-  //      var comment_link = post.find('.meta .comment-count');
-  //      comment_link.after(watchers_link);
-  //      comment_link.after(forks_link);
-  //    }
-  //  });
-  //});
 });
