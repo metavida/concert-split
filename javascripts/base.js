32d46386aecd8f10cf4006f43511ca68f7ef1172
Marcos Wright Kuhns <metavida@hotmail.com>
2012-02-26 18:17:34 -0800

    Switch (mostly) to GitHub v3 API

--- javascripts/base.js
+++ javascripts/base.js
@@ -19,7 +19,7 @@ CSP = {
   getJSON: function(url, callback) {
     waiting_on++;
     $.getJSON(url, function(data) {
-      try { callback(data); } catch(err) {}
+      try { callback(data.data); } catch(err) {}
       waiting_on--;
     });
   },
@@ -63,80 +63,73 @@ jQuery(document).ready(function($) {
     return false;
   };
   
-  var commit_url = 'http://github.com/api/v2/json/commits/list/metavida/concert-split/master?callback=?',
-    tree_url = 'http://github.com/api/v2/json/tree/show/metavida/concert-split/',
-    blob_url = 'http://github.com/api/v2/json/blob/show/metavida/concert-split/',
+  var tree_url = 'https://api.github.com/repos/metavida/concert-split/git/trees/master',
+    blob_url = 'https://github.com/api/v2/json/blob/show/metavida/concert-split/',
     concerts_el = $('#concerts'),
     loading_el = $('#concerts_loading'),
     html = '',
     waiting_on_a = 0;
   waiting_on = 0;
   
-  // Get the most recent commit so we know the SHA of the root tree
-  CSP.getJSON(commit_url, function(data) {
-    //console.log(data);
-    
-    // Loop over the list of root directories, which should NPR shows.
-    CSP.getJSON(tree_url + (data.commits[0].tree) +'?callback=?', function(show_data) {
-      //console.log(show_data);
-      data_tree = show_data.tree;
-      $.each(show_data.tree, function(show) {
-        show = show_data.tree[show];
-        if(show.type == 'tree') {
-          concerts_el.append('<h3>'+ show.name +'</h3><ul></ul>');
-          concerts_el.find('h3').last().append(loading_el);
-          var ul_el = concerts_el.find('ul').last();
-          
-          // Loop over directories for a single show, which should be individual concerts.
-          CSP.getJSON(tree_url + show.sha +"?callback=?", function(concert_data) {
-            sort_f = function(a, b) { try {
-              a = a.name.match(/\d+-\d+-\d/)[0];
-              b = b.name.match(/\d+-\d+-\d/)[0];
-              if(a > b)
-                return -1;
-              else if(a < b)
-                return 1;
-              else
-                return 0;
-            } catch(err) { return 0; } };
-            concert_data = concert_data.tree.sort(sort_f);
-            $.each(concert_data, function(concert) {
-              concert = concert_data[concert];
-              if(concert.type == 'tree') {
-                // Add a placeholder li (to preserve order)
-                ul_el.append('<li id="concert_'+concert.sha+'"></li>');
-                
-                // Get the details for this concert.
-                CSP.getJSON(tree_url + concert.sha +"?callback=?", function(file_data) {
-                  //console.log(file_data);
-                  //console.log(concert);
-                  var has_labels = false,
-                    set_sha = false,
-                    li_el = ul_el.find('li#concert_'+concert.sha);
-                  $.each(file_data.tree, function(file) {
-                    file = file_data.tree[file];
-                    if(file.type == 'blob') {
-                      if(file.name.match(/Audacity Labels/i)) {
-                        has_labels = file.sha;
-                      } else if(file.name.match(/Set List/i)) {
-                        set_sha = file.sha;
-                      }
+  CSP.getJSON(tree_url + '?callback=?', function(show_data) {
+    //console.log(show_data);
+    data_tree = show_data.tree;
+    $.each(show_data.tree, function(show) {
+      show = show_data.tree[show];
+      if(show.type == 'tree') {
+        concerts_el.append('<h3>'+ show.path +'</h3><ul></ul>');
+        concerts_el.find('h3').last().append(loading_el);
+        var ul_el = concerts_el.find('ul').last();
+        
+        // Loop over directories for a single show, which should be individual concerts.
+        CSP.getJSON(show.url + "?callback=?", function(concert_data) {
+          sort_f = function(a, b) { try {
+            a = a.path.match(/\d+-\d+-\d/)[0];
+            b = b.path.match(/\d+-\d+-\d/)[0];
+            if(a > b)
+              return -1;
+            else if(a < b)
+              return 1;
+            else
+              return 0;
+          } catch(err) { return 0; } };
+          concert_data = concert_data.tree.sort(sort_f);
+          $.each(concert_data, function(concert) {
+            concert = concert_data[concert];
+            if(concert.type == 'tree') {
+              // Add a placeholder li (to preserve order)
+              ul_el.append('<li id="concert_'+concert.sha+'"></li>');
+              
+              // Get the details for this concert.
+              CSP.getJSON(concert.url +"?callback=?", function(file_data) {
+                //console.log(file_data);
+                //console.log(concert);
+                var has_labels = false,
+                  set_sha = false,
+                  li_el = ul_el.find('li#concert_'+concert.sha);
+                $.each(file_data.tree, function(file) {
+                  file = file_data.tree[file];
+                  if(file.type == 'blob') {
+                    if(file.path.match(/Audacity Labels/i)) {
+                      has_labels = file.sha;
+                    } else if(file.path.match(/Set List/i)) {
+                      set_sha = file.sha;
                     }
-                  });
-                  if(has_labels && set_sha) {
-                    li_el.append('<a href="'+ blob_url + has_labels +'">'+ concert.name + '</a>');
-                    //li_el.append(' (<a href="'+ blob_url + set_sha +'">view set list</a>)');
-                    $(li_el).find('a').click(show_download_prompt);
-                  } else {
-                    li_el.append(concert.name);
-                    li_el.append(' (<a href="#contribute">awaiting timestamps</a>)');
                   }
                 });
-              }
-            });
+                if(has_labels && set_sha) {
+                  li_el.append('<a href="'+ blob_url + has_labels +'">'+ concert.path + '</a>');
+                  //li_el.append(' (<a href="'+ blob_url + set_sha +'">view set list</a>)');
+                  $(li_el).find('a').click(show_download_prompt);
+                } else {
+                  li_el.append(concert.path);
+                  li_el.append(' (<a href="#contribute">awaiting timestamps</a>)');
+                }
+              });
+            }
           });
-        }
-      });
+        });
+      }
     });
   });
   
